8736fe97f275f4030e0d003a01f65644c9175e63
Aaron Edward Glenn <53454869+AaronEdwardGlenn@users.noreply.github.com>
2020-09-16 16:34:04 -0700

    added some higher order function examples that return custom functions

--- higher-order-functions.js
+++ higher-order-functions.js
@@ -1,7 +1,30 @@
-//// Higher Order Function Example
+//// Higher Order Function Examples
+
+/// this higher order function takes a factor that a variable can be divided by: 
 
 let divider = (factor) => (x => x / factor)
 
 let halfer = divider(2)
 
+let thirder = divider(3)
+
 console.log(halfer(20))
+console.log(thirder(21))
+
+// this higher order function adds a signature onto a string: 
+
+let addSig = (string) => (signature => string + signature)
+
+let story = addSig('The cows come home. ')
+
+console.log(story('By: Aaron Glenn'));
+
+
+// same as above, add name of developer to a project: 
+let newProject = (project) => (dev => project + dev)
+
+let project1 = newProject('Google Bananas. ')
+
+let addDevToProject1 = project1('Dev: Aaron Glenn')
+
+console.log(addDevToProject1);
